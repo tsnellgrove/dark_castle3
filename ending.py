f649c5b9be67d134f1fb4909efeb668d6dbd91d6
Thomas Snellgrove <tsnellgrove@hotmail.com>
2022-06-30 21:18:51 -0400

    titles implemented in ending()

--- ending.py
+++ ending.py
@@ -5,21 +5,35 @@
 
 
 ### imports ###
+import math
 from static_gbl import descript_dict
 
+titles_dict = {
+		-10: 'Burt the Best Forgotten',
+		0: 'Burt the Boneheaded',
+		10: 'Burt the Beginner',
+		20: 'Burt the Better Than Average',
+		30: 'Burt the Brawny',
+		40: 'Burt the Brainy',
+		50: 'Burt the Benevolent',
+		60: 'Burt the Breathtaking',
+		70: 'Burt the Bodacious',
+		80: 'Burt the Bold, King of Bright Castle'
+}
 
 ### end routine ###
 def end(active_gs):
 		moves = active_gs._state_dict['move_counter']
 		game_ending = active_gs.get_game_ending()
+		score = active_gs.get_score()
 
-##		if score < 0:
-##				title_score = -10
-##		elif score == 0:
-##				title_score = 0
-##		else:
-##				title_score = math.ceil(score / 10) * 10
-##		title = static_dict['titles_dict'][title_score]
+		if score < 0:
+				title_score = -10
+		elif score == 0:
+				title_score = 0
+		else:
+				title_score = math.ceil(score / 10) * 10
+		title = titles_dict[title_score]
 
 		if game_ending == 'death':
 				active_gs.buffer("You have died.")
@@ -27,9 +41,10 @@ def end(active_gs):
 				active_gs.buffer("You have quit.")
 		elif game_ending == 'won':
 				active_gs.buffer("You have won!")
+
 		active_gs.buffer("Your adventure ended after " + str(moves) + " moves.")
 		active_gs.print_score()
-##		buffer("Your title is: " + title)
+		active_gs.buffer("Your title is: " + title)
 		if game_ending == 'won':
 				active_gs.buffer(descript_dict['credits'])
 		active_gs.set_end_of_game(True)
