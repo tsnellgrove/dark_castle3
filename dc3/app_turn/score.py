70e761323ce5285095e335da1c9c3193b5c1a74b
Tom Snellgrove <57505060+tsnellgrove@users.noreply.github.com>
2023-11-16 17:58:26 -0500

    pts dict migrated

--- dc3/app_turn/score.py
+++ dc3/app_turn/score.py
@@ -8,19 +8,20 @@
 from dc3.data.static_gbl import descript_dict
 
 ### score dictionaries and lists ###
-score_val_dict = {
-	'rusty_key' : 5,
-	'main_hall' : 5,
-	'shiny_sword' : 10,
-	'throne_room' : 10,
-	'silver_key' : 5,
-	'kinging_scroll' : 5,
-	'royal_crown' : 10,
-	'hedgehog_broach' : 5,
-	'hedgehog_attack' : -20,
-	'game_won' : 15,
-	'goblin_dead' : 5
-}
+
+# score_val_dict = {
+#	'rusty_key' : 5,
+#	'main_hall' : 5,
+#	'shiny_sword' : 10,
+#	'throne_room' : 10,
+#	'silver_key' : 5,
+#	'kinging_scroll' : 5,
+#	'royal_crown' : 10,
+#	'hedgehog_broach' : 5,
+#	'hedgehog_attack' : -20,
+#	'game_won' : 15,
+#	'goblin_dead' : 5
+#}
 
 item_score_lst = [
 	'rusty_key',
@@ -77,14 +78,16 @@ def score(active_gs):
 	# custom scoring
 	score_key = 'hedgehog_attack'
 	if (not active_gs.map.chk_name_exist('royal_hedgehog') and active_gs.get_points_earned_state(score_key) == False):
-		points = score_val_dict[score_key]
+#		points = score_val_dict[score_key]
+		points = descript_dict['score_val'][score_key]
 		active_gs.update_score(points)
 		active_gs.set_points_earned_state(score_key, True)
 		active_gs.print_score()
 
 	score_key = 'goblin_dead'
 	if (active_gs.map.chk_name_exist('dead_goblin') and active_gs.get_points_earned_state(score_key) == False):
-		points = score_val_dict[score_key]
+#		points = score_val_dict[score_key]
+		points = descript_dict['score_val'][score_key]
 		active_gs.update_score(points)
 		active_gs.set_points_earned_state(score_key, True)
 		active_gs.print_score()
@@ -92,7 +95,8 @@ def score(active_gs):
 	score_key = 'game_won'
 	game_ending = active_gs.get_game_ending()
 	if game_ending == 'won':
-		points = score_val_dict[score_key]
+#		points = score_val_dict[score_key]
+		points = descript_dict['score_val'][score_key]
 		active_gs.update_score(points)
 
 	return
