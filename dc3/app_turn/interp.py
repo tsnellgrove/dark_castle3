3d0532dd90baf15f0486482be966a471cb1eb791
Tom Snellgrove <57505060+tsnellgrove@users.noreply.github.com>
2023-12-11 07:05:39 -0500

    clean-up

--- dc3/app_turn/interp.py
+++ dc3/app_turn/interp.py
@@ -39,7 +39,6 @@ abbreviations_dict = {
 ### help = print help info
 def help(active_gs, option):
 	if option == 'basics':
-#		output = static_dict['help_basics']
 		output = active_gs.io.get_str_nr('help_basics')
 	elif option == 'verbs':
 		output = "Available verbs include: " + ', '.join(known_verbs_lst)
@@ -52,7 +51,6 @@ def help(active_gs, option):
 	elif option == 'articles':
 		output = ("The following articles are supported but not required: " + ', '.join(articles_lst))
 	elif option == 'adjectives':
-#		output = static_dict['help_adjectives']
 		output = active_gs.io.get_str_nr('help_adjectives')
 	elif  option == 'abbreviations':
 		pre_out = "Available abbreviations include: "
@@ -60,26 +58,19 @@ def help(active_gs, option):
 			pre_out = pre_out + key + " = " + abbreviations_dict[key] + ", "
 		output = pre_out[:-2]
 	elif option == 'prepositions':
-#		output = static_dict['help_prepositions']
 		output = active_gs.io.get_str_nr('help_prepositions')
 	elif option == 'read':
-#		output = static_dict['help_read']
 		output = active_gs.io.get_str_nr('help_read')
 	elif option == 'attack':
-#		output = static_dict['help_attack']
 		output = active_gs.io.get_str_nr('help_attack')
 	elif option == 'creatures':
-#		output = static_dict['help_creatures']
 		output = active_gs.io.get_str_nr('help_creatures')
 	elif option == 'debug':
 		if not active_gs.state_dict['debug']:
-#			output = static_dict['help_debug_error']
 			output = active_gs.io.get_str_nr('help_debug_error')
 		else:
-#			output = static_dict['help_debug'] + ', '.join(debug_verb_lst)
 			output = active_gs.io.get_str_nr('help_debug') + ', '.join(debug_verb_lst)
 	else:
-#		output = static_dict['help']
 		output = active_gs.io.get_str_nr('help')
 	active_gs.io.buffer(output)
 
