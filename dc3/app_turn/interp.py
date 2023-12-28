aba8a5a17734d1563e58c0077a3830047e603d4b
Tom Snellgrove <57505060+tsnellgrove@users.noreply.github.com>
2023-12-28 09:16:55 -0500

    comment clean-up

--- dc3/app_turn/interp.py
+++ dc3/app_turn/interp.py
@@ -8,8 +8,6 @@
 from itertools import islice
 
 ### interpreter function language static dictionaries & lists ###
-# one_word_only_lst = ['credits', 'score', 'version', 'wait', 'xyzzy42', 'debug_poke53281,0']
-
 one_word_convert_lst = ['north', 'south', 'east', 'west', 'inventory', 'look', 'stand']
 
 ### help = print help info
@@ -19,7 +17,6 @@ def help(gs, option):
 	elif option == 'verbs':
 		output = "Available verbs include: " + ', '.join(gs.io.get_lst('known_verb_lst'))
 	elif option == 'one-word-commands':
-#		display_one_word_lst = one_word_only_lst.copy()
 		display_one_word_lst = gs.io.get_lst('one_word_only_lst').copy()
 		display_one_word_lst.pop()
 		display_one_word_lst.pop()
@@ -148,10 +145,8 @@ def interpreter(user_input, master_obj_lst):
 	if len(user_input_lst) == 1 and word1 == 'help':
 		gs.io.buff_e('help')
 		return 'help', [word1]
-#	if len(user_input_lst) == 1 and word1 in one_word_only_lst:
 	if len(user_input_lst) == 1 and word1 in gs.io.get_lst('one_word_only_lst'):
 		return 'tru_1word', [word1]
-#	if word1 in one_word_only_lst and len(user_input_lst) > 1:
 	if word1 in gs.io.get_lst('one_word_only_lst') and len(user_input_lst) > 1:
 		return 'error', [f"Burt, there are too many words in that sentence. '{word1}' is a one word command!"]
 
