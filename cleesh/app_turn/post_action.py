4ff9e6839ed6d10fce08ac8baff7af161ecb9698
Tom Snellgrove <tsnellgrove@hotmail.com>
2024-06-24 08:33:45 -0400

    started on post_action()

--- cleesh/app_turn/post_action.py
+++ cleesh/app_turn/post_action.py
@@ -1,10 +1,10 @@
-# program: dark castle
+# program: clessh
 # author: Tom Snellgrove
 # module description: identifies and executes pre-action commands
 
 
 def post_action(gs, case, word_lst):
-	cmd_override = False
+#	cmd_override = False
 	mach_obj_lst = gs.map.hero_rm.get_mach_lst(gs)
 	for obj in mach_obj_lst:
 		if obj.trigger_type == 'post_act_switch':
@@ -14,11 +14,12 @@ def post_action(gs, case, word_lst):
 ##			trig_switch_state_lst.append(obj.trig_switch.switch_state)
 			if obj.trig_check(gs, trig_case, trig_switch_state_lst):
 				_unused, result_name = obj.run_mach(gs)
-				gs.score.disp_score(obj.name, result_name, None, gs)
-
+#				gs.score.disp_score(obj.name, result_name, None, gs)
+				gs.score.disp_score(result_name, obj.name, None, gs)
 		if obj.trigger_type == 'post_act_cmd':
 			if obj.trig_check(gs, case, word_lst):
 				_unused, result_name = obj.run_mach(gs)
-				gs.score.disp_score(obj.name, result_name, None, gs)
+#				gs.score.disp_score(obj.name, result_name, None, gs)
+				gs.score.disp_score(result_name, obj.name, None, gs)
 	return
 
