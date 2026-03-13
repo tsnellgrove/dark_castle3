079f069953bf5502cea3bfc7d7346c52dbe2d83e
Tom Snellgrove <tsnellgrove@hotmail.com>
2026-03-12 21:01:07 -0400

    Q fixed the error

--- cleesh/app_turn/validate.py
+++ cleesh/app_turn/validate.py
@@ -22,7 +22,7 @@ def validate(gs, case, word_lst):
 		return False, False, ""
 
 	# *** command errors ***
-	if case in ['2word', 'prep_no_do', 'prep', 'go', 'action_2word']:
+	if case in ['2word', 'prep_no_do', 'prep', 'go', 'action_2word', 'action_dir']:
 		try:
 			if case == 'action_2word':
 				action_str, do_noun_obj = word_lst
